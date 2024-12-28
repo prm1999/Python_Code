86622c4b870a6566acff723f297babc4b7fb5664
Mourya Pradeep Ramashare <53226424+prm1999@users.noreply.github.com>
2024-12-28 17:15:14 +0530

    using copy paste

--- Code/oops/main_inheritance.py
+++ Code/oops/main_inheritance.py
@@ -61,8 +61,31 @@ class Item:
 
 
 #Inheritance of class
+class Phone(Item): # inheritance of Item class
+    all=[]
+    def __init__(self, name,price,quantity=0,broken_phone=0 ):
+        # Run validation to the recived argument
+        assert price>=0 ,f"Price{rice}should be greater than zero"
+        assert quantity>=0 , f"Quantity {quantity} should be greater than zero"
+        assert broken_phone>=0 , f"Quantity {quantity} should be greater than zero"
 
+        #Assign value to self -- Instance attribute 
+        self.name =name
+        self.price=price
+        self.quantity=quantity   #consructor
+        self.broken_phone= broken_phone
+        #print(f"I am here created a new instance {name},{price},{quantity}")
+
+        #append all the instance-- Action to be created
+        Phone.all.append(self)
+    
 # when you can access the class method then you will directly call the class from the function
+phone1=Phone('Nokia',1000,5,1)
+print(phone1.calculate_price())
+
+
+
+
 Item.instantiate_from_csv()
 
 print(Item.all)
