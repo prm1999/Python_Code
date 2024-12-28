e4f3a32e930a6cb741545a0fe569f39528724625
Mourya Pradeep Ramashare <53226424+prm1999@users.noreply.github.com>
2024-12-28 17:23:40 +0530

    using super keyword

--- Code/oops/main_inheritance.py
+++ Code/oops/main_inheritance.py
@@ -4,7 +4,7 @@ class Item:
     pay_rate=0.8 # That is class attribute
     all=[] # Empty list for all the instnce we have created 
     
-    def __init__(self, name,price,quantity=0 ):
+    def __init__(self, name:str,price:float,quantity=0 ):
         # Run validation to the recived argument
         assert price>=0 ,f"Price{rice}should be greater than zero"
         assert quantity>=0 , f"Quantity {quantity} should be greater than zero"
@@ -63,18 +63,19 @@ class Item:
 #Inheritance of class
 class Phone(Item): # inheritance of Item class
     all=[]
-    def __init__(self, name,price,quantity=0,broken_phone=0 ):
+    def __init__(self, name:str,price:float,quantity=0,broken_phone=0 ):
+        #Call to super function to have access to all the attribute and object from the parent class
+        super().__init__(
+            name,price,quantity
+            )
+
+        
         # Run validation to the recived argument
-        assert price>=0 ,f"Price{rice}should be greater than zero"
-        assert quantity>=0 , f"Quantity {quantity} should be greater than zero"
         assert broken_phone>=0 , f"Quantity {quantity} should be greater than zero"
 
         #Assign value to self -- Instance attribute 
-        self.name =name
-        self.price=price
-        self.quantity=quantity   #consructor
         self.broken_phone= broken_phone
-        #print(f"I am here created a new instance {name},{price},{quantity}")
+        #print(f"I am here created a new instance {name},{price},{quantity},{broken_phone}")
 
         #append all the instance-- Action to be created
         Phone.all.append(self)
@@ -82,16 +83,16 @@ class Phone(Item): # inheritance of Item class
 # when you can access the class method then you will directly call the class from the function
 phone1=Phone('Nokia',1000,5,1)
 print(phone1.calculate_price())
+print(Phone.all)
 
 
 
 
-Item.instantiate_from_csv()
+#Item.instantiate_from_csv()
 
-print(Item.all)
+#print(Item.all)
 
 
-print(Item.is_integer(11.90))
 
 
 
